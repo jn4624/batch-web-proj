1ec907b66c858801557bcdcbeb403c683f534b88
LimJiNa <jn4624@gmail.com>
2023-08-08 22:43:58 +0900

    first commit

---
+++ src/main/java/com/web/batch/BatchWebProjApplication.java
@@ -0,0 +1,13 @@
+package com.web.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchWebProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchWebProjApplication.class, args);
+    }
+
+}
